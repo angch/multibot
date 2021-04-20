4ec6a5badf410f9e13defc4ece48bbce28055b9b
Ang Chin Han <angch@tuxuri.com>
2021-04-20 10:42:44 +0800

    refac/feat: Fake uwu + echo

--- pkg/bothandler/struct.go
+++ pkg/bothandler/struct.go
@@ -4,9 +4,15 @@ package bothandler
 // multiplatform bots work.
 
 type MessageHandler func() string
+type CatchallHandler func(string) string
 
 var Handlers = map[string]MessageHandler{}
+var CatchallHandlers = []CatchallHandler{}
 
 func RegisterMessageHandler(m string, h MessageHandler) {
 	Handlers[m] = h
+}
+
+func RegisterCatchallHandler(h CatchallHandler) {
+	CatchallHandlers = append(CatchallHandlers, h)
 }
