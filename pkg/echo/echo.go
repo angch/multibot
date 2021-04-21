96ad277c9f3a5641790887b4981e17beccc37358
Ivan Tham <pickfire@riseup.net>
2021-04-21 10:28:12 +0800

    riir: uwu
    Switch to use SIMD-enabled uwuify library. Requires -ldflags="-r lib".

--- pkg/echo/echo.go
+++ pkg/echo/echo.go
@@ -1,6 +1,9 @@
 package echo
 
-// (ꈍᴗꈍ)
+// #cgo LDFLAGS: -L../../lib -luwu
+// #include "../../lib/uwu.h"
+import "C"
+
 import (
 	"strings"
 
@@ -11,38 +14,10 @@ func init() {
 	bothandler.RegisterCatchallHandler(UwuHandler)
 }
 
-var echos = map[string]string{
-	"hello": "World!",
-	"o/":    "\\o",
-	"\\o":   "o/",
-}
-
-type fragment struct {
-	From, To string
-}
-
-// FIXME/TODO: spin off a module to use sentiment analysis to respond to messages with
-// choice emojis
-var fragments = []fragment{
-	{"(╯°□°）╯︵ ┻━┻", "┬─┬ノ( º _ ºノ) "},
-	{"O.O", "(^_^)"},
-}
-
 func UwuHandler(input string) string {
-	i := strings.ToLower(input)
-	if strings.Contains(i, "uwu") || strings.Contains(input, "(ꈍᴗꈍ)") {
-		return "(ꈍᴗꈍ)"
+	if strings.Contains(strings.ToLower(input), "uwu") {
+		return C.GoString(C.uwuify(C.CString(input)))
 	}
 
-	r, ok := echos[i]
-	if ok {
-		return r
-	}
-
-	for _, v := range fragments {
-		if strings.Contains(i, v.From) {
-			return v.To
-		}
-	}
 	return ""
 }
