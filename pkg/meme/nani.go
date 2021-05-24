07eab273bfb377d6e25c729667f781bdb95c09a3
wendko <wendko.88@gmail.com>
2021-05-25 04:53:50 +0800

    fix: remove gifLinks

--- pkg/meme/nani.go
+++ pkg/meme/nani.go
@@ -7,13 +7,6 @@ import (
 	"github.com/angch/discordbot/pkg/bothandler"
 )
 
-var gifLinks = []string {
-	"https://tenor.com/La2h.gif",
-	"https://tenor.com/Zx43.gif",
-	"https://tenor.com/bCcZV.gif",
-	"https://tenor.com/14jR.gif",
-}
-
 var responseMap = map[string]string{
 	"omae wa mou shindeiru"	: "Nani?!",
 	"omae wa mo shindeiru"	: "Nani?!",
