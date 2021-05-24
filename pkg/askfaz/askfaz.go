adb7e774811615409c50e317705ed4964a27e956
wendko <wendko.88@gmail.com>
2021-05-24 22:04:12 +0800

    fix: use all the handles

--- pkg/askfaz/askfaz.go
+++ pkg/askfaz/askfaz.go
@@ -1,6 +1,7 @@
 package askfaz
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/angch/discordbot/pkg/bothandler"
@@ -19,7 +20,7 @@ var triggers = []string{
 	"serverless",
 }
 
-var untriggers = []string{
+var handles = []string{
 	"@faz",
 	"@tech_tarik",
 }
@@ -39,14 +40,14 @@ func AskFazHandler(input string) string {
 	}
 
 	uncount := 0
-	for _, v := range untriggers {
+	for _, v := range handles {
 		if strings.Contains(i, v) {
 			uncount++
 		}
 	}
 
 	if count >= 3 && uncount == 0 {
-		return "Good question, @faz, what do you think?"
+		return fmt.Sprintf("Good question, %s, what do you think?", strings.Join(handles[:], "/"))
 	}
 
 	return ""
