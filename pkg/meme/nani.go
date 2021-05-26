5dd9b47785861395eb10280559144de7df9cc479
Ang Chin Han <ang.chin.han@gmail.com>
2021-05-26 10:37:47 +0800

    refac(meme): Do it the go way

--- pkg/meme/nani.go
+++ pkg/meme/nani.go
@@ -33,31 +33,14 @@ func init() {
 	bothandler.RegisterCatchallHandler(ReplyNani)
 }
 
-func getKeys(mapItem map[string]string) []string {
-	keys := make([]string, 0, len(mapItem))
-	for k := range mapItem {
-		keys = append(keys, k)
-	}
-	return keys
-}
-
 func ReplyNani(input string) string {
 	i := strings.ToLower(input)
-	response := ""
-	mapKey := ""
 
 	// Check if input string contains any of the map keys
-	for _, key := range getKeys(responseMap) {
+	for key, value := range responseMap {
 		if strings.Contains(i, key) {
-			mapKey = key
+			return fmt.Sprintf("%s %s", value, asciiExplosion)
 		}
 	}
-
-	value, exists := responseMap[mapKey]
-
-	if exists {
-		response = fmt.Sprintf("%s %s", value, asciiExplosion)
-	}
-
-	return response
+	return ""
 }
