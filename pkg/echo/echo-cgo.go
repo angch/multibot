4eeee8ff6ac1221e7745842554447ff6d01a05fe
Ang Chin Han <ang.chin.han@gmail.com>
2022-03-24 11:00:09 +0800

    refac(echo): Testable echo with cgo/go

--- pkg/echo/echo-cgo.go
+++ pkg/echo/echo-cgo.go
@@ -10,25 +10,11 @@ import "C"
 // (ꈍᴗꈍ)
 import (
 	"strings"
-
-	"github.com/angch/discordbot/pkg/bothandler"
 )
 
-func EchoHandler(request bothandler.Request) string {
-	i := strings.ToLower(request.Content)
+func uwucheck(i string) string {
 	if strings.Contains(i, "uwu") || strings.Contains(i, "(ꈍᴗꈍ)") {
 		return strings.Replace(C.GoString(C.uwuify(C.CString(i))), "uwu", "(ꈍᴗꈍ)", 1)
-	}
-
-	r, ok := echos[i]
-	if ok {
-		return r
-	}
-
-	for _, v := range fragments {
-		if strings.Contains(i, v.From) {
-			return v.To
-		}
 	}
 	return ""
 }
