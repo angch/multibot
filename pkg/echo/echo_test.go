f901915395bd5dfacf2fb7ea138e9e79093ca7a5
Ang Chin Han <ang.chin.han@gmail.com>
2023-05-09 11:29:44 +0800

    fix(echo): Caffeine tuning

--- pkg/echo/echo_test.go
+++ pkg/echo/echo_test.go
@@ -14,6 +14,9 @@ func TestEchoHandler(t *testing.T) {
 	}{
 		{"1", "uwu", "(ꈍᴗꈍ)"},
 		{"2", "Plus caffeine only delays the tiredness. It doesn't prevent it.", "Yes, we know."},
+		{"2", `Taking caffeine is basically borrowing energy from your future self and you need to pay it back with interest (haram lol)…
+		One thing that really motivated me to stop coffee intake is that coffee masks the root problem.
+		For example, you feel tired everyday and you solved it with drinking coffee while actually, the real problem is that you don’t exercise enough or constantly watching youtube before bed or don’t eat healthily.`, "Yes, we know."},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
