f56b3a60d4819ebc1f91c381678fc85d9cd76ab0
Ang Chin Han <ang.chin.han@gmail.com>
2023-07-10 23:19:18 +0800

    feat(spacetraders): Trigger on the right channel (spacetraders) only

--- pkg/engineersmy/data.go
+++ pkg/engineersmy/data.go
@@ -2,14 +2,24 @@ package engineersmy
 
 // FIXME: Put this outside as a file
 
-var KnownChannels = map[string]string{
-	"offtopic": "811472319876562991",
-	"general":  "811472319876562989",
-	"":         "811472319876562989",
+var KnownDiscordChannels = map[string]string{
+	"offtopic":     "811472319876562991",
+	"general":      "811472319876562989",
+	"":             "811472319876562989",
+	"spacetraders": "1127471366501834763",
+	"sandbox":      "846297823624298517",
 }
 
 var KnownTelegramChannels = map[string]int{
 	"offtopic": -1001430213215,
 	"general":  -1001430213215,
-//	"":         -1001430213215,
+	// "":         -1001430213215,
+}
+
+func IsKnownDiscordChannel(channelname string, channel string) bool {
+	id, ok := KnownDiscordChannels[channelname]
+	if ok && id == channel {
+		return true
+	}
+	return false
 }
