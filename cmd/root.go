aaa7ef3e699234f0e9b705c68b1cea6ca2dc89a2
Ang Chin Han <ang.chin.han@gmail.com>
2023-07-14 09:55:01 +0800

    chore: go fmt

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,9 +29,9 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "discordbot",
-	Short: "discordbot for EngineersMY",
-	Long:  `A discordbot for fun and play`,
+	Use:   "multibot",
+	Short: "multibot for EngineersMY",
+	Long:  `A multi platform chatbot for fun and play`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
